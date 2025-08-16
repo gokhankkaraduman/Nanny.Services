cbd53c8027a6dd1f10d1da5d50c4fcc12c9b3046
gokhankkaraduman <gokhan.kkaraduman3@gmail.com>
2025-08-16 23:08:15 +0300

    feat: update UserNav component

--- src/components/Header/UserNav/UserNav.jsx
+++ src/components/Header/UserNav/UserNav.jsx
@@ -1,28 +1,55 @@
 import { NavLink } from "react-router-dom";
+import { FaHome, FaUserNurse, FaUser, FaHeart } from "react-icons/fa";
+import { RiLogoutBoxRLine } from "react-icons/ri";
 import style from "./UserNav.module.css";
-import { FaUser } from "react-icons/fa";
 
 export default function UserNav() {
     return (
         <nav className={style.nav}>
             <ul className={style.list}>
-                <NavLink to="/" className={({ isActive }) => isActive ? `${style.listItem} ${style.active}` : style.listItem}>Home</NavLink>
-                <NavLink to="/nannies" className={({ isActive }) => isActive ? `${style.listItem} ${style.active}` : style.listItem}>Nannies</NavLink>
-                <NavLink to="/favorites" className={({ isActive }) => isActive ? `${style.listItem} ${style.active}` : style.listItem}>Favorites</NavLink>
+                <NavLink 
+                    to="/" 
+                    className={({ isActive }) => isActive ? `${style.listItem} ${style.active}` : style.listItem}
+                >
+                    <FaHome className={style.linkIcon} />
+                    Home
+                </NavLink>
+                <NavLink 
+                    to="/nannies" 
+                    className={({ isActive }) => isActive ? `${style.listItem} ${style.active}` : style.listItem}
+                >
+                    <FaUserNurse className={style.linkIcon} />
+                    Nannies
+                </NavLink>
+                <NavLink 
+                    to="/favorites" 
+                    className={({ isActive }) => isActive ? `${style.listItem} ${style.active}` : style.listItem}
+                >
+                    <FaHeart className={style.linkIcon} />
+                    Favorites
+                </NavLink>
             </ul>
             <div className={style.userInfo}>
                 <ul className={style.userList}>
                     <li className={style.userListItem}>
-                        <p className={style.userName}>
-                            <span className={style.icon}><FaUser/></span>username
-                        </p>
+                        <div className={style.userProfile}>
+                            <div className={style.userAvatar}>
+                                <FaUser className={style.avatarIcon} />
+                            </div>
+                            <div className={style.userDetails}>
+                                <span className={style.userGreeting}>Hello,</span>
+                                <span className={style.userName}>John Doe</span>
+                            </div>
+                        </div>
                     </li>
                     <li className={style.userListItem}>
-                        <button className={style.logoutBtn}>Logout</button>
+                        <button className={style.logoutBtn}>
+                            <RiLogoutBoxRLine className={style.logoutIcon} />
+                            Logout
+                        </button>
                     </li>
                 </ul>
             </div>
         </nav>
     )
 }
-
