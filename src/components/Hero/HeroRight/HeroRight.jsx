129691dad68bac9a038b61ab23f11dce763ad3cd
gokhankkaraduman <gokhan.kkaraduman3@gmail.com>
2025-08-16 23:08:28 +0300

    refactor: adjust HeroRight component

--- src/components/Hero/HeroRight/HeroRight.jsx
+++ src/components/Hero/HeroRight/HeroRight.jsx
@@ -1,17 +1,19 @@
 // HeroRight.jsx
-import { FaCheckCircle } from 'react-icons/fa';
+import { FaCheck  } from 'react-icons/fa';
 import style from './HeroRight.module.css';
 
 export default function HeroRight() {
     return (
         <div className={style.heroRight}>
-            <div className={style.imgContainer}>
-                <div className={style.textContent}>
-                    <p className={style.title}><span> <FaCheckCircle /></span>Experienced nannies</p>
+            <div className={style.textContent}>
+                <span className={style.icon}><FaCheck  /></span>
+                <div className={style.textContainer}>
+                    <p className={style.title}>
+                        Experienced nannies
+                    </p>
                     <p className={style.subtitle}>15,000</p>
                 </div>
             </div>
-            <div className={style.babyImage}></div>
         </div>
     )
 }
