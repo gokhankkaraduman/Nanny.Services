4c42e3aee8b0e17d6eb2ac6fa8a9962573a4c15a
gokhankkaraduman <gokhan.kkaraduman3@gmail.com>
2025-08-16 23:08:08 +0300

    refactor: update App structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,35 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
 import HomePage from '../../pages/HomePage/HomePage.jsx';
+import LoginPage from '../../pages/LoginPage/LoginPage.jsx';
+import RegisterPage from '../../pages/RegisterPage/RegisterPage.jsx';
+import { ThemeProvider } from '../../context/ThemeContext.jsx';
+import Header from '../Header/Header.jsx';
 
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <ThemeProvider>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={
+            <>
+              <HomePage />
+            </>
+          } />
+          <Route path="/login" element={
+            <>
+              <LoginPage />
+            </>
+          } />
+          <Route path="/register" element={
+            <>
+              <RegisterPage />
+            </>
+          } />
+        </Routes>
+      </div>
+    </ThemeProvider>
   )
 }
 
