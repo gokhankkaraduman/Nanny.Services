361b2125ee2f755464eafdb84d49fa94b4d27358
gokhankkaraduman <gokhan.kkaraduman3@gmail.com>
2025-08-16 23:08:24 +0300

    refactor: adjust HeroLeft component

--- src/components/Hero/HeroLeft/HeroLeft.jsx
+++ src/components/Hero/HeroLeft/HeroLeft.jsx
@@ -1,12 +1,14 @@
 import style from './HeroLeft.module.css';
+import { FiArrowUpRight } from "react-icons/fi";
 
 export default function HeroLeft() {
     return (
         <div className={style.heroLeft}>
-            <h1 className={style.title}>Make Life Easier for the Family:</h1>
-            <p className={style.subtitle}>Find Babysitters Online for All Occasions</p>
-            <button className={style.ctaButton}>Get started</button>
-        </div> 
+            <div className={style.textContent}>
+                <h1 className={style.title}>Make Life Easier for the Family:</h1>
+                <p className={style.subtitle}>Find Babysitters Online for All Occasions</p>
+                <button className={style.ctaButton}>Get started<FiArrowUpRight className={style.ctaIcon} /></button>
+            </div>
+        </div>
     )
 }
-
